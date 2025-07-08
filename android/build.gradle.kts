32a3a833e754d4805d63200dd5db79801b8e6ff8
Alien-Et <my-melody1@live.com>
2025-07-09 01:03:35 +0800

    000

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -14,6 +14,11 @@ subprojects {
 }
 subprojects {
     project.evaluationDependsOn(":app")
+    project.tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+        kotlinOptions {
+            jvmTarget = "1.8"
+        }
+    }
 }
 
 tasks.register<Delete>("clean") {
