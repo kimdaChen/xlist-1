a3323d0add414e9bd98c6d6d7bd764c5a8799252
Alien-Et <my-melody1@live.com>
2025-07-09 03:14:36 +0800

    00000

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -3,6 +3,10 @@ allprojects {
         google()
         mavenCentral()
     }
+    // 强制所有 Kotlin 模块使用 Java 11
+    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+        kotlinOptions.jvmTarget = "11"
+    }
 }
 
 val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
